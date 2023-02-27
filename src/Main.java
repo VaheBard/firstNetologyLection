c151c76c4dcea76f810fd2fc0b007fb6c7aaab16
Vahe <vabardeg@gmail.com>
2023-02-28 01:56:54 +0400

    first hw

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Vahe";
+        String surname = "Bardeghyn";
+        int income = 120_000;
+        int spending = 90_000;
+
+        System.out.println(name + " " + surname + ", surplus: " + (income - spending) + " руб");
+    }
+}
